73905fa9415d93569f73a44619769df386180c94
Emil Bohleber <emil.bohleber@brickmakers.de>
2021-10-02 20:19:01 +0200

    stop player faster if user is not pressing anzthing

--- RobotSoftExorcism/Assets/Systems/Player/PlayerBrainComponent.cs
+++ RobotSoftExorcism/Assets/Systems/Player/PlayerBrainComponent.cs
@@ -7,6 +7,7 @@ namespace Systems.Player
     [RequireComponent(typeof(MovementComponent))]
     public class PlayerBrainComponent : GameComponent
     {
-        
+        public float goFriction;
+        public float stopFriction;
     }
 }
