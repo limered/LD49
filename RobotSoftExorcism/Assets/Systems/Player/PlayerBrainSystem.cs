a9e430693d5f9e106af99b2b8e454936194e1ed7
Emil Bohleber <emil.bohleber@brickmakers.de>
2021-10-02 20:19:01 +0200

    stop player faster if user is not pressing anzthing

--- RobotSoftExorcism/Assets/Systems/Player/PlayerBrainSystem.cs
+++ RobotSoftExorcism/Assets/Systems/Player/PlayerBrainSystem.cs
@@ -12,27 +12,42 @@ namespace Systems.Player
         {
             SystemFixedUpdate()
                 .Select(_ => component)
-                .Subscribe(CheckPlayerMovement)
+                .Subscribe(ControlPlayer)
                 .AddTo(component);
         }
 
-        private void CheckPlayerMovement(PlayerBrainComponent player)
+        private static void ControlPlayer(PlayerBrainComponent player)
         {
             var movement = player.GetComponent<MovementComponent>();
+            SetPlayerMovement(movement);
+            StopPlayerIfItIsNotMoving(player, movement);
+        }
+
+        private static void StopPlayerIfItIsNotMoving(PlayerBrainComponent playerBrainComponent, MovementComponent movement)
+        {
+            movement.Friction = movement.Direction.Value == Vector2.zero 
+                ? playerBrainComponent.stopFriction : playerBrainComponent.goFriction;
+        }
+
+        private static void SetPlayerMovement(MovementComponent movement)
+        {
             movement.Direction.Value = Vector2.zero;
-            
+
             if (Input.GetKey(KeyCode.W))
             {
                 movement.Direction.Value += Vector2.up;
             }
+
             if (Input.GetKey(KeyCode.S))
             {
                 movement.Direction.Value += Vector2.down;
             }
+
             if (Input.GetKey(KeyCode.A))
             {
                 movement.Direction.Value += Vector2.left;
             }
+
             if (Input.GetKey(KeyCode.D))
             {
                 movement.Direction.Value += Vector2.right;
