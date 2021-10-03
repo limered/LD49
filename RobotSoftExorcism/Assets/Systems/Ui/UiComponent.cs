b67da66a4a36a60f4d94ffda471e4d0e909a68d6
Emil Bohleber <emil.bohleber@brickmakers.de>
2021-10-03 02:00:05 +0200

    add ui system and prefab to show puke and sway meters

---
+++ RobotSoftExorcism/Assets/Systems/Ui/UiComponent.cs
@@ -0,0 +1,11 @@
+﻿using SystemBase;
+using UnityEngine.UI;
+
+namespace Systems.Ui
+{
+    public class UiComponent : GameComponent
+    {
+        public Text fallMeterText;
+        public Text pukeMeterText;
+    }
+}
