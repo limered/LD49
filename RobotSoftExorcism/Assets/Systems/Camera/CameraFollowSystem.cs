511f1179f14266eeea2ea548058e1227324bc396
Emil Bohleber <emil.bohleber@brickmakers.de>
2021-10-03 03:22:28 +0200

    increase playfield in camera

--- RobotSoftExorcism/Assets/Systems/Camera/CameraFollowSystem.cs
+++ RobotSoftExorcism/Assets/Systems/Camera/CameraFollowSystem.cs
@@ -27,7 +27,7 @@ namespace Systems.Camera
             Vector3 currentPostion = component.transform.position;
             Vector3 distance = _movementComponent.transform.position - currentPostion;
             float velocity = Mathf.Abs(_movementComponent.Velocity.x) > 0.05f ? _movementComponent.Velocity.x : Mathf.Sign(_movementComponent.Velocity.x) * 0.5f;
-            if (Mathf.Abs(distance.x) > 2)
+            if (Mathf.Abs(distance.x) > 4)
             {
                 Debug.Log(velocity);
                 Vector3 newPostion = component.transform.position;
