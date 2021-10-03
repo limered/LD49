e601dff591e241a73709bc14c0d44730a14c5fc0
Emil Bohleber <emil.bohleber@brickmakers.de>
2021-10-03 02:00:05 +0200

    add ui system and prefab to show puke and sway meters

---
+++ RobotSoftExorcism/Assets/Systems/Ui/UiSystem.cs
@@ -0,0 +1,23 @@
+﻿using SystemBase;
+using Systems.Player.Events;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Systems.Ui
+{
+    [GameSystem]
+    public class UiSystem : GameSystem<UiComponent>
+    {
+        public override void Register(UiComponent component)
+        {
+            MessageBroker.Default.Receive<PlayerSwayUpdateEvent>()
+                .Subscribe(e => component.fallMeterText.text = $"Fall Meter: {e.SwayPercent:P}")
+                .AddTo(component);
+
+            MessageBroker.Default.Receive<PlayerPukeUpdateEvent>()
+                .Subscribe(e => component.pukeMeterText.text = $"Puke Meter: {e.PukePercent:P}")
+                .AddTo(component);
+        }
+    }
+}
