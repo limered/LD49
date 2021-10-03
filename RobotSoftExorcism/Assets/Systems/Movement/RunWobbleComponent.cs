a084acabae47812eae03b3d760a94e2fd44fd9ca
Emil Bohleber <emil.bohleber@brickmakers.de>
2021-10-03 03:21:44 +0200

    add player wobble

---
+++ RobotSoftExorcism/Assets/Systems/Movement/RunWobbleComponent.cs
@@ -0,0 +1,19 @@
+﻿using SystemBase;
+using SystemBase.StateMachineBase;
+using UnityEngine;
+
+namespace Systems.Movement
+{
+    [RequireComponent(typeof(MovementComponent))]
+    public class RunWobbleComponent : GameComponent
+    {
+        public float wobbleInterval = 0.02f;
+        public float wobbleFactor = 0.02f;
+        
+        /// otherwise this.gameObject is used
+        public GameObject wobbleTarget;
+
+        public Vector3 wobbleAxis = Vector3.up;
+
+    }
+}
---
+++ RobotSoftExorcism/Assets/Systems/Movement/WobbleSystem.cs
@@ -0,0 +1,34 @@
+﻿using SystemBase;
+using UniRx;
+using UnityEngine;
+using Utils.Plugins;
+using Random = UnityEngine.Random;
+
+namespace Systems.Movement
+{
+    [GameSystem]
+    public class WobbleSystem : GameSystem<RunWobbleComponent>
+    {
+        public override void Register(RunWobbleComponent component)
+        {
+            var movement = component.GetComponent<MovementComponent>();
+            var startTime = Time.time - Random.value * 10000;
+
+            SystemUpdate()
+                .Select(_ => Time.time - startTime)
+                .Select(time =>
+                {
+                    var a = Mathf.Sin(time / component.wobbleInterval) * movement.Velocity.magnitude *
+                        component.wobbleFactor;
+
+                    return Mathf.Abs(a) + a;
+                })
+                .Subscribe(sinus =>
+                {
+                    var target = component.wobbleTarget ? component.wobbleTarget : component.gameObject;
+                    target.transform.localPosition = component.wobbleAxis * sinus;
+                })
+                .AddToLifecycleOf(component);
+        }
+    }
+}
