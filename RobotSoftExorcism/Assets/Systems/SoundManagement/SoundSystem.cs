a8e417921c4cccb2d57daafb8319808d102df4e1
Helen <helen-bohleber@web.de>
2021-10-05 00:56:07 +0200

    add dog sound

--- RobotSoftExorcism/Assets/Systems/SoundManagement/SoundSystem.cs
+++ RobotSoftExorcism/Assets/Systems/SoundManagement/SoundSystem.cs
@@ -27,7 +27,7 @@ namespace Systems.SoundManagement
         private string[] PukeReaction = new []{ "did_he_female", "gross_female", "shame_female"};
         private string[] KickReaction = new[] { "looks_unstable_male", "police_female", "shame_female", "stop_him_female", "stop_male"};
         private string[] PoebelReaction = new[] { "get_lost_male", "calm_down_male", "not_rioting_male", "police_female", "stop_him_female", "stop_male" };
-        private string[] GeneralReaction = new[] {"hammered_male", "is_he_drunk_male", "looks_drunk_female", "same_male", "well_oiled_male", "why_drunk_male"};
+        private string[] GeneralReaction = new[] {"dog_1", "dog_2", "dog_3", "hammered_male", "is_he_drunk_male", "looks_drunk_female", "same_male", "well_oiled_male", "why_drunk_male"};
         public override void Register(SoundComponent component)
         {
             //Music
@@ -100,9 +100,12 @@ namespace Systems.SoundManagement
 
         private void PlayMusic(SoundComponent component, int index)
         {
-            Debug.Log("Play music");
             var audioSource = IoC.Game.GetComponent<AudioSource>();
             audioSource.clip = component.MusicClips[index];
+            if (index == 3)
+            {
+                audioSource.volume = 0.5f;
+            }
             audioSource.Play();
         }
 
