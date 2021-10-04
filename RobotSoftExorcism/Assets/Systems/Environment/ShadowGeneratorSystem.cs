52e92b5d14deb9a94cefc197ce2ad4bb83e55c6e
Emil Bohleber <emil.bohleber@brickmakers.de>
2021-10-04 15:24:07 +0200

    add system for shadow generation
    add

---
+++ RobotSoftExorcism/Assets/Systems/Environment/ShadowGeneratorSystem.cs
@@ -0,0 +1,19 @@
+﻿using SystemBase;
+using UnityEngine;
+
+namespace Systems.Environment
+{
+    [GameSystem]
+    public class ShadowGeneratorSystem : GameSystem<SimpleShadowComponent>
+    {
+        public override void Register(SimpleShadowComponent component)
+        {
+            
+        }
+    }
+
+    public class SimpleShadowComponent : GameComponent
+    {
+        public GameObject model;
+    }
+}
