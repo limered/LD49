c991976c9eb0208fa9a3a7e92aa577a4948ba8f3
Christopher Hätty <haettheadco@gmx.de>
2021-10-04 16:21:02 +0200

    Disabled InspectorScript

--- RobotSoftExorcism/Assets/Systems/BackgroundTrack/Editor/TrackHelperInspector.cs
+++ RobotSoftExorcism/Assets/Systems/BackgroundTrack/Editor/TrackHelperInspector.cs
@@ -1,33 +1,33 @@
-#if UNITY_EDITOR
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEditor;
-
-
-namespace Systems.BackgroundTrack
-{
-    [CustomEditor(typeof(TrackHelper))]
-    public class TrackHelperInspector : Editor
-    {
-        public override void OnInspectorGUI()
-        {
-            DrawDefaultInspector();
-            TrackHelper trackHelper = target as TrackHelper;
-            if (GUILayout.Button("Do"))
-            {
-                Undo.RecordObject(trackHelper, "Do");
-                trackHelper.Do();
-                EditorUtility.SetDirty(trackHelper);
-            }
-            
-            if (GUILayout.Button("Rotate"))
-            {
-                Undo.RecordObject(trackHelper, "Rotate");
-                trackHelper.Rotate(5);
-                EditorUtility.SetDirty(trackHelper);
-            }
-        }
-    }
-}
-#endif
+// #if UNITY_EDITOR
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEditor;
+//
+//
+// namespace Systems.BackgroundTrack
+// {
+//     [CustomEditor(typeof(TrackHelper))]
+//     public class TrackHelperInspector : Editor
+//     {
+//         public override void OnInspectorGUI()
+//         {
+//             DrawDefaultInspector();
+//             TrackHelper trackHelper = target as TrackHelper;
+//             if (GUILayout.Button("Do"))
+//             {
+//                 Undo.RecordObject(trackHelper, "Do");
+//                 trackHelper.Do();
+//                 EditorUtility.SetDirty(trackHelper);
+//             }
+//             
+//             if (GUILayout.Button("Rotate"))
+//             {
+//                 Undo.RecordObject(trackHelper, "Rotate");
+//                 trackHelper.Rotate(5);
+//                 EditorUtility.SetDirty(trackHelper);
+//             }
+//         }
+//     }
+// }
+// #endif
