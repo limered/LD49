97084dd8ddae3a47471e9148fd94367deefaf1da
Emil Bohleber <emil.bohleber@brickmakers.de>
2021-10-04 15:23:22 +0200

    increase coffeCount on coffee drink

--- RobotSoftExorcism/Assets/Systems/Score/ScoreComponent.cs
+++ RobotSoftExorcism/Assets/Systems/Score/ScoreComponent.cs
@@ -6,5 +6,6 @@ namespace Systems.Score
     public class ScoreComponent : GameComponent
     {
         public IntReactiveProperty crime = new IntReactiveProperty(0);
+        public IntReactiveProperty coffeeCount = new IntReactiveProperty(0);
     }
 }
