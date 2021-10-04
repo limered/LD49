5229b2757d906e8faec0c6a35f655fc8b36a8326
Helen <helen-bohleber@web.de>
2021-10-04 18:39:50 +0200

    switch music on scene switch

---
+++ RobotSoftExorcism/Assets/Systems/SoundManagement/SoundComponent.cs
@@ -0,0 +1,10 @@
+using SystemBase;
+using UnityEngine;
+
+namespace Systems.SoundManagement
+{
+    public class SoundComponent : GameComponent
+    {
+        public AudioClip[] MusicClips;
+    }
+}
--- RobotSoftExorcism/Assets/Systems/Ui/CurtainComponent.cs
+++ RobotSoftExorcism/Assets/Systems/Ui/CurtainComponent.cs
@@ -1,5 +1,9 @@
+using GameState.Messages;
 using SystemBase;
+using Systems.SceneManagement.Events;
 using Systems.Score;
+using UniRx;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 using Utils;
 
@@ -40,28 +44,33 @@ namespace Systems.Ui
         {
             ResetCrime();
             SceneManager.LoadScene("StartScene");
+            MessageBroker.Default.Publish(new StartEvent());
         }
         
         private void GoToBar()
         {
             SceneManager.LoadScene("BarScene");
+            MessageBroker.Default.Publish(new BarEnterEvent());
         }
 
         private void GoOutside()
         {
             SceneManager.LoadScene("CityScene");
+            MessageBroker.Default.Publish(new CityEnterEvent());
         }
 
         private void GoToHappyEnd()
         {
             ResetCrime();
             SceneManager.LoadScene("HappyEnd");
+            MessageBroker.Default.Publish(new HomeEnterEvent());
         }
 
         public void GoToSadEnd()
         {
             ResetCrime();
             SceneManager.LoadScene("SadEnd");
+            MessageBroker.Default.Publish(new PrisonEnterEvent());
         }
 
         private void ResetCrime()
