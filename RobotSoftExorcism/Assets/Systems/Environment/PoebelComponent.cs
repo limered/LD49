600c23b3283c886b8a5ac15f03186d82a58265a3
Emil Bohleber <emil.bohleber@brickmakers.de>
2021-10-04 17:35:12 +0200

    add poebel reaction to person

--- RobotSoftExorcism/Assets/Systems/Environment/PoebelComponent.cs
+++ RobotSoftExorcism/Assets/Systems/Environment/PoebelComponent.cs
@@ -1,10 +1,13 @@
 ﻿using SystemBase;
 using UniRx;
+using UnityEngine;
 
 namespace Systems.Environment
 {
     public class PoebelComponent : GameComponent
     {
         public ReactiveCommand WasPoebeledOnTrigger = new ReactiveCommand();
+
+        public Vector2 Target { get; set; }
     }
 }
