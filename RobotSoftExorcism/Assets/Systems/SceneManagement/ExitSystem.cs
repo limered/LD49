f02eed7bee5fad720176b7abbe4e407e8b63f8bf
Helen <helen-bohleber@web.de>
2021-10-04 14:48:21 +0200

    go to next scene depending on current scene

---
+++ RobotSoftExorcism/Assets/Systems/SceneManagement/ExitSystem.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using SystemBase;
+using Systems;
+using Systems.Player;
+using Systems.Player.Events;
+using Systems.Score;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using Utils;
+
+[GameSystem]
+public class ExitSystem : GameSystem<ExitComponent>
+{
+    public override void Register(ExitComponent component)
+    {
+        component.OnTriggerEnterAsObservable().Subscribe(collider => PlayerEntersExit(collider)).AddTo(component);
+    }
+
+    private void PlayerEntersExit(Collider collider)
+    {
+        if (collider.GetComponent<PlayerBrainComponent>())
+        {
+            Debug.Log("hit exit");
+            var coffeeCount = IoC.Game.GetComponent<ScoreComponent>().coffeeCount.Value;
+            MessageBroker.Default.Publish(new PlayerEnterExitEvent());
+        }
+    }
+}
